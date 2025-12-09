7d256989601da7eb16f1599add5081b1b0a22ac9
ShinraT <henric.nilsson01@gmail.com>
2025-12-09 16:16:44 +0100

    added keymousereader

--- Game1.cs
+++ Game1.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 
 namespace Matteuppgift_4_a
 {
@@ -9,17 +10,24 @@ namespace Matteuppgift_4_a
     {
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
+        private Texture2D pixel;
         public Vector4 row1;
         public Vector4 row2;
         public Vector4 row3;
         public Vector4 row4;
         public Matrix rotation;
+        public Vector4 mittPunkt;
+        public Vector4 p1, p2, p3, p4;
+        public List<Vector4> punkter;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            graphics.PreferredBackBufferHeight = 500;
+            graphics.PreferredBackBufferWidth = 500;
+            graphics.ApplyChanges();
         }
 
         protected override void Initialize()
@@ -32,8 +40,20 @@ namespace Matteuppgift_4_a
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            GenerateRotationMatrix(35f);
-            
+            punkter = new List<Vector4>();
+            mittPunkt = new Vector4(250, 250, 1, 1); // Mitten
+            p1 = new Vector4(300, 200, 1, 1); // Uppe höger
+            p2 = new Vector4(200, 200, 1, 1); // Uppe vänster
+            p3 = new Vector4(200, 300, 1, 1); // Nere vänster
+            p4 = new Vector4(300, 300, 1, 1); // Nere höger
+            punkter.Add(mittPunkt);
+            punkter.Add(p1);
+            punkter.Add(p2);
+            punkter.Add(p3);
+            punkter.Add(p4);
+
+            pixel = new Texture2D(GraphicsDevice, 1, 1);
+            pixel.SetData(new[] { Color.White });
         }
         public float GraderTillRadianer(float grader) // Uträkning för att ändra grader till radianer. För att kunna använda det i kod.
         {
@@ -51,10 +71,32 @@ namespace Matteuppgift_4_a
             return rotation;
 
         }
+
+        public void Rotera(float grader)
+        {
+            Matrix rotation = GenerateRotationMatrix(grader);
+
+            for (int i = 0; i < punkter.Count; i++)
+            {
+                punkter[i] = RoteraPunkterna(punkter[i], rotation);
+            }
+        }
+
+        public static Vector4 RoteraPunkterna(Vector4 p, Matrix m)
+        {
+            return new Vector4(
+                p.X * m.M11 + p.Y * m.M12 + p.Z * m.M13 + p.W * m.M14,
+                p.X * m.M21 + p.Y * m.M22 + p.Z * m.M23 + p.W * m.M24,
+                p.X * m.M31 + p.Y * m.M32 + p.Z * m.M33 + p.W * m.M34,
+                p.X * m.M41 + p.Y * m.M42 + p.Z * m.M43 + p.W * m.M44
+            );
+        }
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            
 
             // TODO: Add your update logic here
 
@@ -63,7 +105,14 @@ namespace Matteuppgift_4_a
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
+            spriteBatch.Begin();
+
+            foreach(var pos in punkter)
+            {
+                spriteBatch.Draw(pixel, new Vector2(pos.X, pos.Y), null, Color.BlanchedAlmond, 0f, Vector2.Zero, new Vector2(5f, 5f), SpriteEffects.None, 0f);
+            }
+            spriteBatch.End();
 
             // TODO: Add your drawing code here
 
---
+++ KeyMouseReader.cs
@@ -0,0 +1,44 @@
+﻿using Matteuppgift_4_a;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+using System.Collections;
+namespace Matteuppgift_4_a
+{
+    static class KeyMouseReader
+    {
+        public static KeyboardState keyState, oldKeyState = Keyboard.GetState();
+        public static MouseState mouseState, oldMouseState = Mouse.GetState();
+        public static Rectangle MouseRectangle;
+        public static bool KeyPressed(Keys key)
+        {
+            return keyState.IsKeyDown(key) && oldKeyState.IsKeyUp(key);
+        }
+        public static bool LeftClick()
+        {
+            return mouseState.LeftButton == ButtonState.Pressed && oldMouseState.LeftButton == ButtonState.Released;
+        }
+        public static bool RightClick()
+        {
+            return mouseState.RightButton == ButtonState.Pressed && oldMouseState.RightButton == ButtonState.Released;
+        }
+        public static bool AnyKeyDown()
+        {
+            return keyState.GetPressedKeys().Length > 0;
+        }
+
+        //Should be called at beginning of Update in Game
+        public static void Update()
+        {
+            oldKeyState = keyState;
+            keyState = Keyboard.GetState();
+            oldMouseState = mouseState;
+            mouseState = Mouse.GetState();
+            MouseRectangle = new Rectangle(mouseState.X, mouseState.Y, 32, 32);
+
+        }
+    }
+}
